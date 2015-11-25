e0b8138aa067387162ba47de846667a24580f315
Ian K <ian.kawabata@gmail.com>
2015-11-25 08:35:16 -0800

    Create motivation_station.py

---
+++ motivation_station.py
@@ -0,0 +1,5 @@
+# outline of project
+# - wait for input from user + attract mode led
+# - on input play jingle mp3 + led animation
+# - then play a random mp3 from a specific folder and play a randomly selected led animation
+# - at end of mp3 stops all leds then returns to attract mode
