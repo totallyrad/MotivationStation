545608800fb024dd4b9a3d79fc83d72c5caea7c6
Ian K <ian.kawabata@gmail.com>
2016-02-14 16:46:58 -0800

    Update motivation_2.py

--- motivation_2.py
+++ motivation_2.py
@@ -10,11 +10,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def my_callback(channel):  
-
-
   randomfile = random.choice(os.listdir("/home/pi/Motivation"))
   file = '/home/pi/Motivation/' + randomfile
-
   os.system('sudo mpg321 ' + file)
-  
+
+GPIO.add_event_detect(23, GPIO.FALLING, callback=my_callback, bouncetime=300)
+
+
+
   
