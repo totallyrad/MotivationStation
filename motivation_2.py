6a91f1d9cd5f28942e961db316622506e1df8178
Ian K <ian.kawabata@gmail.com>
2016-02-14 16:45:03 -0800

    Update motivation_2.py

--- motivation_2.py
+++ motivation_2.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python
 
+from time import sleep
+import os, random
+import RPi.GPIO as GPIO
+
+#setup buttons as inputs
+GPIO.setmode(GPIO.BCM)
+#Sets up GPIO pins
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def my_callback(channel):  
+
+
+  randomfile = random.choice(os.listdir("/home/pi/Motivation"))
+  file = '/home/pi/Motivation/' + randomfile
+
+  os.system('sudo mpg321 ' + file)
+  
+  
