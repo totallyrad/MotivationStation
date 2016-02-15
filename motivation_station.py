1091a11fcb18ebeed85a6a0f5104df90d680e8aa
Ian K <ian.kawabata@gmail.com>
2016-02-14 16:58:42 -0800

    Update motivation_station.py

--- motivation_station.py
+++ motivation_station.py
@@ -8,7 +8,7 @@ import RPi.GPIO as GPIO
 #setup buttons as inputs
 GPIO.setmode(GPIO.BCM)
 #sSets up GPIO pins
-GPIO.setup(18, GPIO.IN)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 #sets "playing" to false
 #this lets us stop the player
@@ -26,23 +26,23 @@ while True:
         #if a sound is not playing do this
         if ( playing == 0):
                 #if someone pressons button one play one file
-                        if ( GPIO.input(18) == False ):
-                        randomfile = random.choice(os.listdir("/home/pi/Motivation"))
-                        file = '/home/pi/Motivation/' + randomfile
+                        if ( GPIO.input(23) == False ):
+                                randomfile = random.choice(os.listdir("/home/pi/Motivation"))
+                                file = '/home/pi/Motivation/' + randomfile
 
-                        os.system('sudo mpg321 ' + file)
-                        #set 'playing' value to 'true'
-                        playing = 1
+                                os.system('sudo mpg321 ' + file)
+                                #set 'playing' value to 'true'
+                                playing = 1
 
 
         #if a sound is not playing do this
         else:
                 #if either button is pressed and a sound is playing STOP IT!
-                if( GPIO.input(18) == False):
+                if( GPIO.input(23) == False):
                         #stops the sound
                         os.system('sudo pkill -SIGKILL mpg321 #force exit')
                         #sets my "playing" variable to false
                         playing=0
         #waits half of a second before starting over again
         #at the top of my while loop
-        sleep(0.5);
+        sleep(0);
